67142f27f7015e6518d8932e76b9cb1b802b1509
Plimon Howard <plimon.howard@gmail.com>
2020-12-09 21:00:06 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,1 +1,3 @@
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INT, subgenre_id INT);
+CREATE TABLE subgenres (id INT PRIMARY KEY, title TEXT);
+CREATE TABLE authors (id INT PRIMARY KEY, name TEXT);
